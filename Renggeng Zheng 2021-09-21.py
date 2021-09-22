596b0418c8d9ab98733c25fffea40939d479e317
Renggeng Zheng <64385210+rengzhengcodes@users.noreply.github.com>
2021-09-22 08:38:04 -0400

    Added header

--- Renggeng Zheng 2021-09-21.py
+++ Renggeng Zheng 2021-09-21.py
@@ -1,3 +1,9 @@
+#Renggeng Zheng
+#SoftDev
+#K03 -- SoftDev Name Printing 
+#2021-09-21 
+
+
 ##enables debug print statements
 DEBUG = True
 ##if this exists as a txt you can import it, but we do not have a txt, json, csv, etc of names
@@ -9,7 +15,7 @@ pd1.sort()
 pd2.sort()
 
 if DEBUG:
-    print(pd1, pd2) #checks that sort works in DEBUG mode
+	print(pd1, pd2) #checks that sort works in DEBUG mode
 
 pd_chosen = input("What period's name do you want? ") #allows CLI input for which period
 pd_chosen = int(pd_chosen) #converts input into an integer
@@ -17,6 +23,8 @@ name_index = input("Input the index of the name you wish to retrieve (0-based): 
 name_index = int(name_index) #converts input into an integer
 
 if pd_chosen == 1:
-    print("Name: " + pd1[name_index])
+	print("Name: " + pd1[name_index])
 else:
-    print("Name: " + pd2[name_index])
+	print("Name: " + pd2[name_index])
+
+##exec("""print("Name: " + pd""" + str(pd_chosen) + "[" + str(name_index) + "])") #would've preferred a dict but specifications say use 2 vars
