701b18138f9ca48843665bfc12bcb4f8ce5a913e
rengzhengcodes <rzhengcontacts@gmail.com>
2021-09-28 15:13:49 -0400

    Added proper printout comparisons. Total is 100 : 99.8 but that is expected as we included "Other" while the real CSV only did so implicitly

--- 06_Py/test.py
+++ 06_Py/test.py
@@ -2,17 +2,23 @@
 import main
 
 results = dict()
-results["total"] = 100000
-for run in range(results["total"]):
+results["Total"] = 100000
+for run in range(results["Total"]):
 	output = main.random_job()
 	if output in results.keys():
 		results[output] += 1
 	else:
 		results[output] = 1
 
-print("Job, Percentage") #format of print statements
+print("Job, Percentage, Expected") #format of print statements
 sorted_jobs = list(results.keys())
 sorted_jobs.sort()
 for job in sorted_jobs:
-	print(job + " : " +  str(results[job] / results["total"] * 100))
+	if job == "Total": #want to print this at the end for formatting clarity
+		continue
+	if job != "Other": #necessary because the job "Other" is not explicitly defined in the original
+		print(job + " : " + str(results[job] / results["Total"] * 100) + " : " + str(main.job_percentages[job]))
+	else:
+		print(job + " : " + str(results[job] / results["Total"] * 100) + " : " + "0.2")
 
+print("Total" + " : " + str(results["Total"] / results["Total"] * 100) + " : " + str(main.job_percentages["Total"]))
