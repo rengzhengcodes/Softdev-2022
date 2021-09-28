e730ac6d21d1e9521039dd17dbb02557b0581031
rengzhengcodes <rzhengcontacts@gmail.com>
2021-09-28 17:21:18 -0400

    changed make new folder algorithm

--- templates/classwork/create_folder.sh
+++ templates/classwork/create_folder.sh
@@ -1,6 +1,6 @@
 echo "which assignment is this: "
 read assignment_num
-directory="$assignment_num""_Py"
+directory="$assignment_num""_py"
 # works if you run from root directory
 mkdir "$directory"
 #rsync used because cp does not work
