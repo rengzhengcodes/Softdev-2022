bf7e0adece98a5384db502afd52f410b9cc0f5d5
rengzhengcodes <rzhengcontacts@gmail.com>
2021-09-28 15:35:01 -0400

    added comments

--- 06_Py-csv/main.py
+++ 06_Py-csv/main.py
@@ -3,6 +3,31 @@
 #K06 -- Reading from a CSV
 #2021-09-28
 
+# Findings
+#1 Knowing a library well and its functionality is better with documentation
+## than stabbing blindly in the dark.
+#2 Doing frequent run checks is good so that you make sure that you aren't
+## working with code that you will have to scrap later because it just doesn't
+## work.
+#3 Commit frequently in case you need to go back.
+
+## CSV Specific Findings:
+#1 The CSV dictreader likes to read it in with the first row as a header for
+## the columns of every subsequent node. Very annoying if you don't want that.
+#2 Basically an excel spreadsheet with each "cell" being which newline its
+## on and how many commas in it is.
+
+# Testing
+#1 Testing is very important to verify code integrity. While it might work
+## nominally on the surface, there may be hidden, unconsidered complications.
+#2 You can import a *.py file in the same directory with a simple import
+## statement. This imports both the functions and any defined variables.
+#4 All the code in a *.py file (except if it is inside of a function
+## declaration is run at the time the file is imported.
+##1 Function declarations are also run, but the contents the function is
+### wrapping do not.
+##2 Basically like the exec() command.
+
 #Debug variable
 DEBUG = False
 #DEBUG PRINTER
