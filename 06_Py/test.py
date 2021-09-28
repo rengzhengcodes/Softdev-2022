92d16c098253622185d5e96063d61f6fe1d0746d
rengzhengcodes <rzhengcontacts@gmail.com>
2021-09-28 13:56:51 -0400

    probabilities test run. Need to remove total from dict

--- 06_Py/test.py
+++ 06_Py/test.py
@@ -1,6 +1,13 @@
+# imports our main file
 import main
 
 results = dict()
 results["total"] = 100000
 for run in range(results["total"]):
-	print(main.random_job())
+	output = main.random_job()
+	if output in results.keys():
+		results[output] += 1
+	else:
+		results[output] = 1
+
+print(results)
