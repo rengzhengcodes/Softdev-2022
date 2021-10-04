59e23a312b344404fc3f9907f6e2c0acb995bb70
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-04 12:06:23 -0400

    More comments

--- 10_flask/v3/app.py
+++ 10_flask/v3/app.py
@@ -13,4 +13,17 @@ def hello_world():
     return "No hablo queso!"
 
 app.debug = True
+# True means we turned on website debug mode.
+# EDIT: it said it did, but what did it do?
+# Starting printout changed to:
+	#"* Serving Flask app 'app' (lazy loading)
+	# * Environment: production
+	#   WARNING: This is a development server. Do not use it in a production deployment.
+	#   Use a production WSGI server instead.
+	# * Debug mode: on
+	# * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+	# * Restarting with stat
+	# * Debugger is active!
+	# * Debugger PIN: 125-144-047
+	#"
 app.run()
