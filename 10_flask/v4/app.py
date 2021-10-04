f1047f8507a2621736125938022f036b53e10442
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-04 12:08:09 -0400

    added comments for v4

--- 10_flask/v4/app.py
+++ 10_flask/v4/app.py
@@ -3,6 +3,12 @@
 # K10 - Flask Learning Day 2, the Flaskening
 # 2021-10-04
 
+# Nothing has changed output-wise from V3.
+# For V4 it now seems that it only runs app if the python file is the main one called. EDIT: true
+# Debugger stuff still not figured out. EDIT: currently still true
+# It runs nothing if you just import the file from another file. Edit: true
+
+
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
