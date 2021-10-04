a5423ea8692a2502f6ab3d97135c6135670d9d82
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-04 12:03:28 -0400

    tabified + comments added

--- 10_flask/v1/app.py
+++ 10_flask/v1/app.py
@@ -7,7 +7,10 @@ from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
+# seems to tell the code that when a user accesses the website's root (website.com/, it runs the function)
+# EDIT: Confirmed
 def hello_world():
-    return "No hablo queso!"
+	# Printouts seem the same despite removal of print(__name__) for all tested methods. EDIT: It's false we just didn't see it.
+	return "No hablo queso!"
 
 app.run()
