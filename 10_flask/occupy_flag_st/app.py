ed6a8ae5640d8a3b9b15207c18acb6959d3299ab
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-04 12:28:45 -0400

    fixed kwargs()

--- 10_flask/occupy_flag_st/app.py
+++ 10_flask/occupy_flag_st/app.py
@@ -2,12 +2,13 @@ from flask import Flask #imports the flask class
 from occupation_generator import random_job, job_percentages #imports the function that generates the random job + the weighted probabilities
 
 app = Flask(__name__) #creates an instance of the Flask class
+@app.route("/")
 def main():
 	job = random_job()
 	jobs = str(job_percentages.keys())
-	return
-	"""EAR (Edwin Zheng, Angela Zheng, Renggeng Zheng) \n
+	template =  """EAR (Edwin Zheng, Angela Zheng, Renggeng Zheng) \n
 	Random Occupation: {occupation} \n
-	Possible Occupations: {jobs}""".format(occupation = job, jobs = jobs) #template string that then gets formatted via kwargs
-
+	Possible Occupations: {jobs}"""
+	template = template.format(occupation = job, jobs = jobs) #template string that then gets formatted via kwargs
+	return template
 app.run()
