54ffe82feb731ce119a0ba52e202eab5bde8f1e1
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-04 12:20:18 -0400

    created a baseline framework that might work

---
+++ 10_flask/occupy_flag_st/app.py
@@ -0,0 +1,11 @@
+from flask import Flask #imports the flask class
+from occupation_generator import random_job, job_percentages #imports the function that generates the random job + the weighted probabilities
+
+app = Flask(__name__) #creates an instance of the Flask class
+def main():
+	job = random_job()
+	jobs = str(job_percentages.keys())
+	return
+	"""EAR (Edwin Zheng, Angela Zheng, Renggeng Zheng) \n
+	Random Occupation: {occupation} \n
+	Possible Occupations: {jobs}""".format(occupation = job, jobs = jobs) #template string that then gets formatted via kwargs
