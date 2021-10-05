40c6e7582621488c089ef46d5859c51c7e01810e
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-05 12:19:22 -0400

    added random tester file

---
+++ 10_flask/occupy_flask_st/test.py
@@ -0,0 +1,24 @@
+# imports our main file
+import occupation_generator as main
+
+results = dict()
+results["Total"] = 10000000
+for run in range(results["Total"]):
+	output = main.random_job()
+	if output in results.keys():
+		results[output] += 1
+	else:
+		results[output] = 1
+
+print("Job, Percentage, Expected") #format of print statements
+sorted_jobs = list(results.keys())
+sorted_jobs.sort()
+for job in sorted_jobs:
+	if job == "Total": #want to print this at the end for formatting clarity
+		continue
+	if job != "Other": #necessary because the job "Other" is not explicitly defined in the original
+		print(job + " : " + str(round(results[job] / results["Total"] * 100, 2)) + " : " + str(main.job_percentages[job]))
+	else:
+		print(job + " : " + str(round(results[job] / results["Total"] * 100, 2)) + " : " + "0.2")
+
+print("Total" + " : " + str(results["Total"] / results["Total"] * 100) + " : " + str(main.job_percentages["Total"]))
