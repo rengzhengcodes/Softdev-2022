c2bff4efdbb70fa5f51223028a51d63e4907d374
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-05 08:39:15 -0400

    changed to random library weighted probabilities

--- 10_flask/occupy_flask_st/occupation_generator.py
+++ 10_flask/occupy_flask_st/occupation_generator.py
@@ -49,21 +49,12 @@ for row in occupations:
 
 #generates a random job given a probability dict formatted {job: percentage with total at the bottom}
 def random_job(probability_book = job_percentages): #default argument provided. Valid for an assignment use case.
-	if DEBUG:
-		random.seed(a=42) #sets seed for predictable testing
+	# removes total from dictionary
+	try: #deletes total line if it exists
+		del probability_book["Total"]
+	except KeyError:
+		pass
+	jobs = list(probability_book.keys())
+	percentages = list(probability_book.values())
 
-	#upper bound is 100 as despite the exclusive end, we are including 0 in our set of rng numbers
-	#Step is 0.1 because that's the accuracy to which the data is to.
-	number_rolled = random.randrange(0, 1000, 1) / 10 #randrange does not accept floats as inputs, so we make do
-	debug_print(number_rolled)
-	#sum of probabilities of jobs iterated through. Uses DND esque DC check, with a job's given DC/valid range
-	#being the end of the previous job's upper bound and its own upper bound.
-	current_dc = 0
-	debug_print(probability_book)
-	for job, probability in probability_book.items(): #grabs the job and its associated probability
-		current_dc += probability #adds the currrent probability to the probability DC
-		# strictly less than because the first number has the segment from 0 to its number.
-		# if we did <= the rest of the segments would be off by 0.1 as the first has the inclusive 0 as well.
-		if number_rolled < current_dc and job != "Total":
-			return job #DC is reached, which means the number generated corresponds to thsi job, so return it.
-	return "Other" #our thing does not neatly add up to 100%, as 0.2% of people work in other industries. This is the failthrough case.
+	return random.choices(population=jobs, k=1, weights=percentages)
