f42d5192a94753cc59c25bd91c23abda71a8b215
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-09 11:21:25 -0400

    changed occupations.py to work with new dict structure given by reqs

--- 13_tempwork/occupations.py
+++ 13_tempwork/occupations.py
@@ -8,54 +8,54 @@ import random
 
 
 def read_occupations(filename: str) -> dict:
-    """Reads a CSV file containing job classes and percentages and returns a
-    dictionary with the job class as the key and the percentage as a float.
-    Ignores the first header line and adds an 'Other' category for percentages
-    outside of the 'Total'."""
+	"""Reads a CSV file containing job classes and percentages and returns a
+	dictionary with the job class as the key and the percentage as a float.
+	Ignores the first header line and adds an 'Other' category for percentages
+	outside of the 'Total'."""
 
-    occupations = {}
-    with open(filename, newline="") as csvfile:
-        reader = csv.reader(csvfile)
+	occupations = {}
+	with open(filename, newline="") as csvfile:
+		reader = csv.reader(csvfile)
 
-        # We ignore the first header line with the column titles.
-        next(reader)
+		# We ignore the first header line with the column titles.
+		next(reader)
 
-        for row in reader:
-            job_class = row[0]
-            percentage = row[1]
+		for row in reader:
+			job_class = row[0]
+			percentage = row[1]
 			link = row[2]
-            occupations[job_class] = {"percentage" = float(percentage), "link" = link}
+			occupations[job_class] = {"percentage": float(percentage), "link": link}
 
-    # We mark everything not in the occupations list as "Other".
-    total_percentage = occupations["Total"]
-    occupations["Other"] = round(100 - total_percentage, 2)
-    del occupations["Total"]
+	# We mark everything not in the occupations list as "Other".
+	total_percentage = occupations["Total"]["percentage"]
+	occupations["Other"] = {"percentage": round(100 - total_percentage, 2), "link": occupations["Total"]["link"]}
+	del occupations["Total"]
 
-    return occupations
+	return occupations
 
 
 def choose_from_dict(occupations: dict) -> str:
-    """Picks an occupation randomly using the percentage weights in the given
-    occupations dictionary."""
+	"""Picks an occupation randomly using the percentage weights in the given
+	occupations dictionary."""
 
-    job_classes = list(occupations.keys())
-    percentages = list(occupations.values())
+	job_classes = list(occupations.keys())
+	percentages = list(value["percentage"] for value in occupations.values())
 
-    choice = random.choices(job_classes, weights=percentages)[0]
-    return choice
+	choice = random.choices(job_classes, weights=percentages)[0]
+	return choice
 
 
 def random_occupation(filename: str) -> str:
-    """Returns a random occupation based on the job classes and percentage
-    weights provided in the given CSV file."""
+	"""Returns a random occupation based on the job classes and percentage
+	weights provided in the given CSV file."""
 
-    occupations = read_occupations(filename)
-    return choose_from_dict(occupations)
+	occupations = read_occupations(filename)
+	return choose_from_dict(occupations)
 
 
 def main():
-    print(random_occupation("occupations.csv"))
+	print(random_occupation("data/occupations.csv"))
 
 
 if __name__ == "__main__":
-    main()
+	main()
