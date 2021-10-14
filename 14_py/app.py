d211935c3e53544da1d19f3d10632f5802005176
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-14 08:20:47 -0400

    pre run comments

--- 14_py/app.py
+++ 14_py/app.py
@@ -1,6 +1,6 @@
 # Clyde 'Thluffy' Sinclair
 # SoftDev
-# Oct 2021 
+# Oct 2021
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -26,36 +26,36 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) # the servers
     print("***DIAG: request obj ***")
-    print(request)
+    print(request) #object the server gets whenever it is pinged
     print("***DIAG: request.args ***")
-    print(request.args)
+    print(request.args) #dict of inputs (all inputs)
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
-    print(request.headers)
+    print(request.headers) # request shipping label. A request is basically a big box of mail, the header is the thing the post service uses to deliver the mail.
     return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
+def authenticate(): # runs when a form is submitted
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
     print("***DIAG: request obj ***")
     print(request)
     print("***DIAG: request.args ***")
-    print(request.args)
+    print(request.args) #request.args has the form data in the form of a dictionary
     #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    #print(reque0st.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
     return "Waaaa hooo HAAAH"  #response to a form submission
 
 
-    
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
