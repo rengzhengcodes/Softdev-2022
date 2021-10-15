33b9683c21741442f56294f66d1aadb0b5e48b71
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-15 06:19:29 -0400

    tabs

--- 14_form/app.py
+++ 14_form/app.py
@@ -4,14 +4,14 @@
 #2021-10-14
 
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+from flask import Flask						 #facilitate flask webserving
+from flask import render_template	 #facilitate jinja templating
+from flask import request					 #facilitate form submission
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
-app = Flask(__name__)    #create Flask object
+app = Flask(__name__)		#create Flask object
 
 
 '''
@@ -28,38 +28,38 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 #disabling GET means you can't request this function.
 #Disabling POST you CAN request the function BUT it's not allowed to return anything (or does not know how).
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app) # the servers
-    print("***DIAG: request obj ***")
-    print(request) #object the server gets whenever it is pinged
-    print("***DIAG: request.args ***")
-    print(request.args) #dict of inputs (all inputs)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers) # request shipping label. A request is basically a big box of mail, the header is the thing the post service uses to deliver the mail.
-    return render_template( 'login.html' )
+		print("\n\n\n")
+		print("***DIAG: this Flask obj ***")
+		print(app) # the servers
+		print("***DIAG: request obj ***")
+		print(request) #object the server gets whenever it is pinged
+		print("***DIAG: request.args ***")
+		print(request.args) #dict of inputs (all inputs)
+		#print("***DIAG: request.args['username']	***")
+		#print(request.args['username'])
+		print("***DIAG: request.headers ***")
+		print(request.headers) # request shipping label. A request is basically a big box of mail, the header is the thing the post service uses to deliver the mail.
+		return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate(): # runs when a form is submitted
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args) #request.args has the form data in the form of a dictionary
-    #print("***DIAG: request.args['username']  ***")
-    #print(reque0st.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template('response.html', username = request.args['username'], request = request.headers)  #response to a form submission
+		print("\n\n\n")
+		print("***DIAG: this Flask obj ***")
+		print(app)
+		print("***DIAG: request obj ***")
+		print(request)
+		print("***DIAG: request.args ***")
+		print(request.args) #request.args has the form data in the form of a dictionary
+		#print("***DIAG: request.args['username']	***")
+		#print(reque0st.args['username'])
+		print("***DIAG: request.headers ***")
+		print(request.headers)
+		return render_template('response.html', username = request.args['username'], request = request.headers)	#response to a form submission
 
 
 
 if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True
-    app.run()
+		#enable debugging, auto-restarting of server when this file is modified
+		app.debug = True
+		app.run()
