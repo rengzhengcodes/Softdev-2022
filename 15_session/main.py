f6984662ad3c894904084e05f5e3e451e89d03da
rengzhengcodes <rzhengcontacts@gmail.com>
2021-10-18 09:07:04 -0400

    comments

--- 15_session/main.py
+++ 15_session/main.py
@@ -12,15 +12,15 @@ header = """#Team02 - Renggeng Zheng Ivan Lam Lia Nelson"""
 @server.route('/', methods=["POST", "GET"])
 def main():
 	'''displays login page'''
-	return render_template("login.html", header=header)
+	return render_template("login.html", header=header) # renders our login with our header
 
 @server.route('/auth', methods=["POST", "GET"])
 def authenticate():
 	'''autenticates login info'''
 	if request.form["u_name"] == "admin" and request.form["p_word"] == "admin":
-		return render_template("response.html", header=header)
+		return render_template("response.html", header=header, username = request.form["u_name"]) # user greeting + our header
 	else:
-		return render_template("login.html", header=header, login_status="Username or PW is incorrect.")
+		return render_template("login.html", header=header, login_status="Username or PW is incorrect.") #returns the index page if the login isn't correct + tells the user the input wasn't right
 
 if __name__ == "__main__": #false if this file imported as module
 	#enable debugging, auto-restarting of server when this file is modified
