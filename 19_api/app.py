9894fb6f93e17ae24f7b658009a07278dc59a4fe
rengzhengcodes <rzhengcontacts@gmail.com>
2021-11-23 08:15:23 -0500

    template

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,4 +1,16 @@
 #Renggeng Zheng
 #SoftDev
-#K<nn> -- <Title/Topic/Summary>
-#<yyyy>-<mm>-<dd>
+#K19 -- Using Rest APIs - NASA
+#2021-11-23
+
+from flask import Flask, render_template
+import requests
+app = Flask(__name__) 
+
+@app.route("/")       
+def index():
+	return render_template("main.html", pic = pic)
+
+if __name__ == "__main__":  # true if this file NOT imported
+	app.debug = True        # enable auto-reload upon code change
+	app.run()
