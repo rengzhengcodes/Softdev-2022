60b7ce4bad26f25c45c03bd890037f1cb4c2e458
rengzhengcodes <rzhengcontacts@gmail.com>
2021-11-23 21:11:24 -0500

    working requests

--- 19_api/app.py
+++ 19_api/app.py
@@ -11,9 +11,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	request = urllib.request(open("key_nasa.txt", -r))
-	print(request)
-	return render_template("main.html", pic = pic, )
+	url = "https://api.nasa.gov/planetary/apod?api_key="
+	url += str(open("key_nasa.txt", "r").read())
+	request = urllib.request.urlopen(url)
+	data = json.loads(request.read())
+	return render_template("main.html", pic=data['url'], explanation=data['explanation'])
 
 if __name__ == "__main__":  # true if this file NOT imported
 	app.debug = True        # enable auto-reload upon code change
