5a4ece3861c039eb5938a3befefa889bdf223310
rengzhengcodes <rzhengcontacts@gmail.com>
2022-02-08 08:23:22 -0500

    tabs

--- 28/dommy_consolery.js
+++ 28/dommy_consolery.js
@@ -1,21 +1,21 @@
 /*
-   your PPTASK:
+	 your PPTASK:
 
-   Test drive each bit of code in this file,
-    and insert comments galore, indicating anything
-     you discover,
-    	have questions about,
-    		or otherwise deem notable.
+	 Test drive each bit of code in this file,
+		and insert comments galore, indicating anything
+		 you discover,
+			have questions about,
+				or otherwise deem notable.
 
-    		Write with your future self or teammates in mind.
+				Write with your future self or teammates in mind.
 
-    		If you find yourself falling out of flow mode, consult
-    		other teams
-    		MDN
+				If you find yourself falling out of flow mode, consult
+				other teams
+				MDN
 
-   A few comments have been pre-filled for you...
+	 A few comments have been pre-filled for you...
 
-   (delete this block comment once you are done)
+	 (delete this block comment once you are done)
 */
 
 // Team Z :: Mark Zhu, Renggeng Zheng
@@ -35,53 +35,53 @@ var j = 20;
 
 //assign an anonymous fxn to a var
 var f = function(x) {
-  var j=30;
-  return j+x;
+	var j=30;
+	return j+x;
 };
 
 
 //instantiate an object
 var o = { 'name' : 'Thluffy',
-          age : 15,
-          items : [10, 20, 30, 40],
-          morestuff : {a : 1, b : 'ayo'},
-          func : function(x) {
-            return x+30;
-          }
-        };
+		age : 15,
+		items : [10, 20, 30, 40],
+		morestuff : {a : 1, b : 'ayo'},
+		func : function(x) {
+			return x+30;
+		}
+	};
 
 
 var addItem = function(text) {
-  var list = document.getElementById("thelist");
-  var newitem = document.createElement("li");
-  newitem.innerHTML = text;
-  list.appendChild(newitem);
+	var list = document.getElementById("thelist");
+	var newitem = document.createElement("li");
+	newitem.innerHTML = text;
+	list.appendChild(newitem);
 };
 
 
 var removeItem = function(n) {
-  var listitems = document.getElementsByTagName('li');
-  listitems[n].remove();
+	var listitems = document.getElementsByTagName('li');
+	listitems[n].remove();
 };
 
 
 var red = function() {
-  var items = document.getElementsByTagName("li");
-  for(var i = 0; i < items.length; i++) {
-    items[i].classList.add('red');
-  }
+	var items = document.getElementsByTagName("li");
+	for(var i = 0; i < items.length; i++) {
+		items[i].classList.add('red');
+	}
 };
 
 
 var stripe = function() {
-  var items = document.getElementsByTagName("li");
-  for(var i = 0; i < items.length; i++) {
-    if (i%2==0){
-      items[i].classList.add('red');
-    } else {
-      items[i].classList.add('blue');
-    }
-  }
+	var items = document.getElementsByTagName("li");
+	for(var i = 0; i < items.length; i++) {
+		if (i%2==0){
+			items[i].classList.add('red');
+		} else {
+			items[i].classList.add('blue');
+		}
+	}
 };
 
 //insert your implementations here for...
